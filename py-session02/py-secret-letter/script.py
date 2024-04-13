0f72f1eadb70d0687694ef7cf82b724b45680b8e
EVDV90 <evd.clases@gmail.com>
2024-04-13 09:52:12 +0200

    Version V1.0

---
+++ py-session02/py-secret-letter/script.py
@@ -0,0 +1,12 @@
+'''
+Main module: The secret letter
+'''
+
+import datetime
+import random
+import time
+
+print("*" * 150)
+print("S E C R E T      L E T T E R       G A M E")
+print("*" * 150) 
+
---
+++ py-session02/py-secret-number/script.py
@@ -0,0 +1,12 @@
+'''
+Main module: The secret letter
+'''
+
+import datetime
+import random
+import time
+
+print("*" * 150)
+print("S E C R E T      N U M B E R       G A M E")
+print("*" * 150) 
+
